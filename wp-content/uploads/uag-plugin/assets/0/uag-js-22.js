c9cdc0367e4d5edb65da0b0935995fa16545314a
neonnightsnz <nzneonnights@gmail.com>
2026-08-29 23:06:56 +1200

    i

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-22.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"2dc76752","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"office@wordpress-1413591-5262009.cloudwaysapps.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Form Submission","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Please fill up the above captcha.","confirmationUrl":""}, '.uagb-block-2dc76752', 22 );
+});
+ });
